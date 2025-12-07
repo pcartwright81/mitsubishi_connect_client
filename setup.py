0b3c3119706b71db351c8262fc50eeb0c2423e12
Patrick Cartwright <pcartwright1981@gmail.com>
2025-12-07 08:52:42 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with Path("README.md").open() as fh:
 
 setuptools.setup(
     name="mitsubishi_connect_client",
-    version="0.0.6",
+    version="0.0.7",
     author="Patrick Cartwright",
     author_email="pcartwright1981@gmail.com",
     description="Client for the Mitsubishi Connect API",
@@ -23,3 +23,4 @@ setuptools.setup(
         "Operating System :: OS Independent",
     ],
 )
+
